06058781f1bb24647d4f2ca140b2b23ee7f69efa
rhpidfyre <brandon@rhpidfyre.io>
2025-12-09 00:30:18 -0500

    Gallery photo enhancer v1

--- src/components/Gallery/Enhancer.svelte
+++ src/components/Gallery/Enhancer.svelte
@@ -20,23 +20,53 @@
 </script>
 
 <script>
-	import { fade } from "svelte/transition";
+	import { scale, fade } from "svelte/transition";
+
+	const trans_duration: number = 300;
 </script>
 
 {#if image_enhancer.visible}
-	<main transition:fade={{ duration: 150 }}>
-		<div>
+	<main transition:fade={{ duration: trans_duration }}>
+		<div
+			class="image-window"
+			transition:scale={{ duration: trans_duration }}
+		>
 			<header>
 				<p>{image_enhancer.image_name}</p>
-				<button onclick={() => image_enhancer.close()}>X</button>
+				<div class="actions">
+					<!-- download button -->
+					<a title="Download" href={image_enhancer.image} download
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 -960 960 960"
+							height="100%"
+							width="100%"
+							fill="#e3e3e3"
+							><path
+								d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"
+							/></svg
+						></a
+					>
+					<!-- close button -->
+					<button title="Close" onclick={() => image_enhancer.close()}
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 -960 960 960"
+							height="100%"
+							width="100%"
+							fill="#e3e3e3"
+							><path
+								d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+							/></svg
+						></button
+					>
+				</div>
 			</header>
 			<section>
-				<img
-					src={image_enhancer.image}
-					alt={image_enhancer.image_name}
-				/>
-				<a href={image_enhancer.image} download>DOWNLOAD</a>
-				<button onclick={() => image_enhancer.close()}>CLOSE</button>
+				<div
+					class="image-large"
+					style={`background-image: url(${image_enhancer.image});`}
+				></div>
 			</section>
 		</div>
 	</main>
@@ -44,6 +74,8 @@
 
 <style>
 	main {
+		--header-height: 50px;
+
 		display: grid;
 		justify-content: center;
 		align-items: center;
@@ -54,12 +86,7 @@
 		position: fixed;
 		backdrop-filter: blur(10px) brightness(30%);
 	}
-	div {
-		background-color: var(--element-clr-even);
-		height: 90vh;
-		width: 90vw;
-		border: 1px solid var(--special-clr);
-	}
+
 	header {
 		--margin: 10px;
 
@@ -67,30 +94,49 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		height: 50px;
+		height: var(--header-height);
 		width: 100%;
+
+		button,
+		a {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: transparent;
+			font-weight: bold;
+			margin-right: var(--margin);
+			border: 0;
+			padding: 0;
+			width: 40px;
+			height: 40px;
+		}
 
 		p {
 			font-weight: bold;
 			margin-left: var(--margin);
 		}
-		button {
-			font-weight: bold;
-			margin-right: var(--margin);
-			border: 0;
-			border-radius: 100%;
-			width: 30px;
-			height: 30px;
+
+		.actions {
+			display: flex;
+			gap: 5px;
 		}
 	}
-	section {
-		display: grid;
-		justify-content: center;
 
-		img {
-			max-width: 90%;
-			max-height: 90%;
-			margin-inline: auto;
-		}
+	section {
+		width: 100%;
+		height: 100%;
+	}
+
+	.image-window {
+		height: inherit;
+		width: inherit;
+	}
+
+	.image-large {
+		width: 100%;
+		height: calc(100% - var(--header-height));
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: contain;
 	}
 </style>
