d2b2b028aa274c48319ffbc2d14155f663430768
rhpidfyre <brandon@rhpidfyre.io>
2026-02-12 23:14:53 -0500

    Header button styles and the header floats

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,9 +1,27 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import ExternalLink from "./externalLink";
+
+interface NavigationButton {
+	to: string;
+	children: string;
+}
+
+function NavigationButton({ to, children }: NavigationButton) {
+	const current_route = useLocation().pathname === to;
+
+	return (
+		<Link
+			to={to}
+			className={current_route ? "bg-selection-alpha text-white" : ""}
+		>
+			{children}
+		</Link>
+	);
+}
 
 function Navigation() {
 	return (
-		<div className="bg-element-dark w-full h-18 flex justify-between items-center border-b border-b-[#333333] p-5">
+		<div className="bg-element-dark w-full h-18 flex justify-between items-center border border-[#333333] rounded-2xl p-5">
 			{/*Left - Logo*/}
 			<Link
 				to="/"
@@ -14,15 +32,16 @@ function Navigation() {
 					alt="Home"
 					className="h-13 rounded-md"
 				/>
-				<h1>Koconut MC</h1>
+				<h1 className="text-[#999999]">Koconut MC</h1>
 			</Link>
+
 			{/*Right - Nav Buttons*/}
-			<div className="[&_a:hover]:bg-selection-alpha [&_a:hover]:text-white [&_a]:p-3 [&_a]:rounded-2xl text-[#b3b3b3] text-md flex justify-center items-center gap-4">
-				<Link to="/">Home</Link>
-				<Link to="/about">About</Link>
-				<Link to="/modpack">Modpack</Link>
-				<Link to="/gallery">Gallery</Link>
-				<Link to="/commands">Commands</Link>
+			<div className="[&_a:hover]:bg-selection-alpha [&_a:hover]:text-white [&_a]:p-3 [&_a]:rounded-2xl flex justify-center items-center gap-4 text-[#b3b3b3] text-md">
+				<NavigationButton to="/">Home</NavigationButton>
+				<NavigationButton to="/about">About</NavigationButton>
+				<NavigationButton to="/modpack">Modpack</NavigationButton>
+				<NavigationButton to="/gallery">Gallery</NavigationButton>
+				<NavigationButton to="/commands">Commands</NavigationButton>
 				<ExternalLink href="https://map.koconutmc.com/">
 					Map
 				</ExternalLink>
@@ -33,7 +52,7 @@ function Navigation() {
 
 function Header() {
 	return (
-		<header className="flex flex-col">
+		<header className="flex flex-col w-250 mx-auto my-5">
 			{/*Header navigation bar*/}
 			<Navigation />
 			{/*Logo*/}
@@ -42,7 +61,7 @@ function Header() {
 					<img
 						src="/koconutmc.webp"
 						alt="Home"
-						className="h-60 my-30 hover:scale-108 transition-transform duration-400"
+						className="h-55 my-30 hover:scale-108 transition-transform duration-400"
 					/>
 				</Link>
 			</div>
